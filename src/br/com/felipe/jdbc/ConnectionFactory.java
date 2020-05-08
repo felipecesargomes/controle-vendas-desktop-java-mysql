9388f11d27869e9635dc2876606b8058a3d7f84a
Felipe César <www.felipecesargomes@gmail.com>
2020-05-08 15:11:01 -0300

    Criando Pool de Conexão Simples com um teste de conexão

---
+++ src/br/com/felipe/jdbc/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package br.com.felipe.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdvendas?useTimezone=true&serverTimezone=UTC","vendas","123");
+		} catch(Exception erro) {
+			throw new RuntimeException(erro);
+		}
+	}
+	
+}
